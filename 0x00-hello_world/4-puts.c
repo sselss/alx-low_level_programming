056a27eb3fd40134f832e5ce50b1a6dfa7f871ba
Sele <selegot@gmail.com>
2023-09-07 16:03:36 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,12 @@
 /**
  * main - it prints out by puts
  *
- * return (0)
+ * Return: always  (0)
  *
  */
 int main (void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return(0);
 }
 
