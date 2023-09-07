541edb5577372589ee1da5f8c79b31c7c8ba6de2
Sele <selegot@gmail.com>
2023-09-07 15:55:46 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - it prints out by puts
+ *
+ * return (0)
+ *
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzl\ne");
+	return (0);
+}
+
