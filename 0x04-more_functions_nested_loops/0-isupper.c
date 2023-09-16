9fc9d528c4cf3d4c77e78896f909edce89b07863
Sele <selegot@gmail.com>
2023-09-15 21:09:11 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase char
+ * @c: function parameter
+ * Return: ! if its uppercase
+ */
+
+int _ispper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else	
+		return (0);
+}
